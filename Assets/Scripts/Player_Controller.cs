b855f28a13a408c52c48e75b4606789b2a29fb53
Ritchie Hofmann <rhofma2@lsu.edu>
2015-03-15 19:41:04 -0500

    First Commit. This creates basic project folder for the rest of team to pull from and begin working on their scenes. Only level 0 has any work. Level 0 is not functional.

---
+++ Assets/Scripts/Player_Controller.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable] 
+public class Boundary 
+{ public float xMin, xMax, yMin, yMax; }
+
+public class Player_Controller : MonoBehaviour {
+	public float speed;
+	public Boundary boundary;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void FixedUpdate()  
+	{
+		//pick up movement from user input
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		float moveVertical = Input.GetAxis("Vertical");
+		
+		//calculates where and how quickly the ship moves
+		Vector2 movement = new Vector2 (moveHorizontal, moveVertical);
+		GetComponent<Rigidbody2D>().velocity = movement * speed;
+		
+		GetComponent<Rigidbody2D>().position = new Vector2
+			(
+				Mathf.Clamp(GetComponent<Rigidbody2D>().position.x, boundary.xMin, boundary.xMax), 
+				Mathf.Clamp(GetComponent<Rigidbody2D>().position.y, boundary.yMin, boundary.yMax)
+			);	
+	}
+}
