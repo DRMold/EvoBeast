668f37aa607257272781b9f77c35a0e4b25c7bbd
unknown <jmederna@uno.edu>
2015-04-13 22:44:05 -0500

    Josh's overworld

---
+++ Assets/Scripts/OverworldPlayerController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OverworldPlayerController : MonoBehaviour {
+
+	public float speed;
+
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis("Horizontal");
+		
+		Vector2 movement = new Vector2(moveHorizontal, 0);
+		
+		GetComponent<Rigidbody2D>().AddForce(movement * speed * Time.deltaTime);
+	}
+}
