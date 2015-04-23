ea81517d4664649204ebef05d75dfba02fdb3043
unknown <jmederna@uno.edu>
2015-04-22 19:56:58 -0500

    Added start menu

---
+++ Assets/Scripts/StartMenu.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+// Programmed by Joshua Medernach
+
+public class StartMenu : MonoBehaviour {
+
+	// A place to refer to our buttons
+	public Button start;
+	public Button levelLoad;
+	public Button credits;
+	public Button quit;
+
+	// Use this for initialization
+	void Start () {
+		start = start.GetComponent<Button> ();
+		levelLoad = levelLoad.GetComponent<Button> ();
+		credits = credits.GetComponent<Button> ();
+		quit = quit.GetComponent<Button> ();
+	}
+
+	// The start button is pressed
+	public void StartOverworld() {
+		Application.LoadLevel (1); // This loads the Overworld scene
+	}
+
+	// The quit button is pressed
+	public void QuitGame() {
+		Application.Quit ();
+	}
+}
